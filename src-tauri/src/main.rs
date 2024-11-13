fc684a1f679f6121bfdc51d5c8516abaa09064da
Lucien Thomas <lucienthomas00@gmail.com>
2024-11-13 15:12:12 -0600

    dev: update with better template. rounded edges etc

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            window_shadows::set_shadow(window, true).unwrap();
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
